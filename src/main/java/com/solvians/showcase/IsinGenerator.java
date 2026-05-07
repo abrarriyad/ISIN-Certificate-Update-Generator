9b28919bf1adad3e718fc67c24d7ffdfb018c05a
abrarriyad <rashidabrarriyad@gmail.com>
2026-05-07 13:53:59 +0600

    Isin generation added

---
+++ src/main/java/com/solvians/showcase/IsinGenerator.java
@@ -0,0 +1,23 @@
+package com.solvians.showcase;
+
+public class IsinGenerator {
+
+    private static final String UPPER_CASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String ALPHANUMERIC_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    public String generate(){
+        String randomUpperCaseLetters = Utils.generateRandomString(UPPER_CASE_LETTERS,2);
+        String randomAlphaNums = Utils.generateRandomString(ALPHANUMERIC_CHARS,9);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(randomUpperCaseLetters).append(randomAlphaNums);
+
+        System.out.println(sb);
+
+        int checkDigit = Utils.calculateCheckDigit(sb.toString());
+
+        System.out.println(checkDigit);
+
+        return sb.append(checkDigit).toString();
+    }
+}
---
+++ src/main/java/com/solvians/showcase/Utils.java
@@ -0,0 +1,72 @@
+package com.solvians.showcase;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Utils {
+
+    public static ThreadLocalRandom getRandomValue(){
+        return ThreadLocalRandom.current();
+    }
+
+    public static String generateRandomString(String input, int length){
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0; i<length; i++){
+            int randomIndex = getRandomValue().nextInt(input.length());
+            System.out.println(randomIndex);
+            sb.append(input.charAt(randomIndex));
+        }
+        return sb.toString();
+
+    }
+
+    public static int calculateCheckDigit(String input){
+            String num = convertLettersToNumbers(input);
+            int sum = calculateSum(num);
+            int reminder = sum%10;
+
+            if(reminder ==0){
+                return 0;
+            }
+
+            return 10 - reminder;
+    }
+
+    public static String convertLettersToNumbers(String input){
+        StringBuilder output = new StringBuilder();
+
+        for(char ch: input.toCharArray()){
+            if(Character.isLetter(ch)){
+                output.append(ch-'A'+10);
+            } else {
+                output.append(ch);
+            }
+        }
+
+        return output.toString();
+    }
+
+    public static int calculateSum(String input){
+            int sum =0;
+            int length = input.length();
+
+            for(int i=input.length()-1; i>=0; i--){
+                char currentChar = input.charAt(i);
+                int digit = Character.getNumericValue(currentChar);
+                int position = length - i;
+
+                if(position %2 ==1){
+                    digit = digit*2;
+
+                    if(digit >9){
+                        digit = digit / 10 + digit % 10;
+                    }
+                }
+                sum+=digit;
+            }
+
+            return sum;
+    }
+
+
+}
